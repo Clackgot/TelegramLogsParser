8d21ea83b50e887100592137d09523171d262248
clackgot <clackgot@gmail.com>
2022-01-09 02:58:02 +0300

    Добавьте файлы проекта.

---
+++ TelegramParser/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TelegramParser.ViewModels;
+
+namespace TelegramParser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+
+        private List<string> dirLogsWithTelegram = new List<string>();
+
+        public IEnumerable<string> GetDirsWithTelegram(string path)
+        {
+            string[] logs = new string[0];
+            try
+            {
+                logs = Directory.GetDirectories(path);
+            }
+            catch
+            {
+                Console.WriteLine("Не удалось получить список директорий");
+
+                yield break;
+            }
+
+            foreach (var log in logs)
+            {
+                bool isTelegramExists = Directory.Exists($"{log}\\Telegram");
+                if (isTelegramExists)
+                {
+                    yield return log;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Событие нажатия на кнопку "Открыть папку"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonOpenDir_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                var path = dialog.SelectedPath;
+
+                int dirsCount = Directory.GetDirectories(path).Count();
+
+                dirLogsWithTelegram = GetDirsWithTelegram(path).ToList();
+                int dirsWithTelegramCount = dirLogsWithTelegram.Count();
+
+                ((MainWindowViewModel)DataContext).LogsCount = dirsCount;
+                ((MainWindowViewModel)DataContext).WithTelegram = dirsWithTelegramCount;
+                ((MainWindowViewModel)DataContext).WithoutTelegram = dirsCount - dirsWithTelegramCount;
+            }
+
+
+        }
+        /// <summary>
+        /// Событие нажатия на кнопку "Копировать найденные папки"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ButtonSaveDirs_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            int copiedCount = 0;
+            int dirLogsWithTelegramCount = dirLogsWithTelegram.Count();
+
+
+            double progressBarValue = 0;
+
+            int errorCount = 0;
+
+
+            foreach (var dir in dirLogsWithTelegram)
+            {
+                var sourceDirectory = $"{dir}\\Telegram";
+                var logName = sourceDirectory.Split('\\').TakeLast(2).First().Trim();
+                var destinationDirectoryName = $"output\\{logName}";
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        FileSystem.CopyDirectory(sourceDirectory, destinationDirectoryName);
+                        copiedCount += 1;
+                        progressBarValue = (double)(((double)copiedCount / (double)dirLogsWithTelegramCount) * 100);
+                    }
+                    catch(Exception e)
+                    {
+                        errorCount++;
+                    }
+                });
+                progressBar.Value = progressBarValue;
+            }
+
+
+            if(errorCount == dirLogsWithTelegramCount)
+            {
+                MessageBox.Show($"Не удалось скопировать {errorCount} папок", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (errorCount > 0)
+            {
+                MessageBox.Show($"Cкопировано [{dirLogsWithTelegramCount-errorCount}/{dirLogsWithTelegramCount}] папок", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                MessageBox.Show($"Скопировано {dirLogsWithTelegramCount} папок", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
---
+++ TelegramParser/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TelegramParser.ViewModels.Base;
+
+namespace TelegramParser.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private int logsCount = 0;
+        public int LogsCount
+        {
+            get { return logsCount; }
+            set { Set(ref logsCount, value); }
+        }
+
+        private int withTelegram = 0;
+        public int WithTelegram
+        {
+            get { return logsCount; }
+            set { Set(ref logsCount, value); }
+        }
+
+        private int withoutTelegram = 0;
+        public int WithoutTelegram
+        {
+            get { return logsCount; }
+            set { Set(ref logsCount, value); }
+        }
+
+        private string title = "Telegram parser";
+        public string Title
+        {
+            get { return title; }
+            set { Set(ref title, value); }
+        }
+    }
+}
